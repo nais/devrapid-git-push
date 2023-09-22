df7c2b6ef2d9e79036ab40a627e4f61f85c34b3d
J-K. Solbakken <jksolbakken@gmail.com>
2023-09-22 08:42:09 +0200

    chore: upped some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.google.protobuf.gradle.*
 
 plugins {
-    kotlin("jvm") version ("1.9.0")
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version ("1.9.10")
+    kotlin("plugin.serialization") version "1.9.10"
     id("com.google.protobuf") version "0.9.4"
     id("org.cyclonedx.bom") version "1.7.4"
     application
@@ -28,14 +28,14 @@ configurations {
     }
 }
 
-val junitVersion = "5.9.3"
-val ktorVersion = "2.3.2"
+val junitVersion = "5.10.0"
+val ktorVersion = "2.3.4"
 val log4jVersion = "2.20.0"
 val assertJVersion = "3.24.2"
 val prometheusVersion = "0.16.0"
-val micrometerVersion = "1.11.2"
+val micrometerVersion = "1.11.4"
 val serializerVersion = "0.20.0"
-val protobufVersion = "3.23.4"
+val protobufVersion = "3.24.3"
 
 
 
@@ -47,14 +47,14 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-    implementation("com.vlkan.log4j2:log4j2-logstash-layout-fatjar:0.21.3")
+    implementation("com.vlkan.log4j2:log4j2-logstash-layout-fatjar:1.0.5")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializerVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializerVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serializerVersion")
     implementation("io.prometheus:simpleclient:$prometheusVersion")
     implementation("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
     implementation("commons-codec:commons-codec:1.16.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     implementation("org.apache.kafka:kafka-clients:3.5.0")
     implementation("io.confluent:kafka-protobuf-serializer:7.4.1")
     api("com.google.protobuf:protobuf-java:$protobufVersion")
